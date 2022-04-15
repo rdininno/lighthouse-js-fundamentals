00e9c6ed40c2a787c72f996b168b6d7ba632f5fa
Robert Di Ninno <robertdininno@gmail.com>
2022-04-15 02:05:22 +0000

    choose school with conditions based on age

---
+++ conditionals-whichSchool.js
@@ -0,0 +1,11 @@
+const whichSchool  = function (age) {
+  if (age < 13){
+     console.log("Elementary School");
+  } else if (age <=18){
+     console.log("Secondary School");
+  } else {
+    console.log("Lighthouse Labs");
+  }
+}
+
+console.log(whichSchool(13));
