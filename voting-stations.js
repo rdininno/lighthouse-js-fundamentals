0fd4c10302c9af5eb5bd55ebb468abe0db364801
Robert Di Ninno <robertdininno@gmail.com>
2022-04-16 03:09:02 +0000

    checking an array of locations to see if they fit the criteria of a polling station.

---
+++ voting-stations.js
@@ -0,0 +1,27 @@
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+
+
+const chooseStations = function (stations) {
+  let goodStations = [];
+  for (const station of stations) {
+    const capacity = station[1];
+
+    if (capacity >= 20) {
+      const kind = station[2];
+
+      if (kind === 'school' || kind === 'community centre') {
+        console.log(station[0]);
+        goodStations.push(station[0]);
+      }
+
+    }
+  }
+  return goodStations;
+}
+
+console.log(chooseStations(stations));
