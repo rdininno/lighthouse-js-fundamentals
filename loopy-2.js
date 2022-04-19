2dced8bfd08dfa89fef68125a537be2622df6df9
Robert Di Ninno <robertdininno@gmail.com>
2022-04-19 03:04:18 +0000

    loopy lighthouse 2

--- loopy-2.js
+++ loopy-2.js
@@ -1,13 +1,13 @@
-function loopyLighthouse(range, multiples, words){
+function loopyLighthouse(range, multiples, words) {
   let start = range[0];
   let end = range[1];
 
-  for(let i = start; i < end + 1; i++){
-    if(i % multiples[0] ===0 && i % multiples[1] === 0){
+  for (let i = start; i < end + 1; i++) {
+    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
       console.log(words[0] + words[1]);
-    } else if ( i % multiples[0] === 0 ){
+    } else if (i % multiples[0] === 0) {
       console.log(words[0]);
-    } else if ( i % multiples[1] === 0){
+    } else if (i % multiples[1] === 0) {
       console.log(words[1]);
     } else {
       console.log(i);
